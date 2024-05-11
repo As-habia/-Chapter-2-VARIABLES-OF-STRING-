3ade522bda5058252104ea83a384ea12f76b046b
As-habia <afaanmehmil9>
2024-05-11 12:44:53 -0700

    JS Chap 2

---
+++ app.js
@@ -0,0 +1,67 @@
+
+//task 1
+var username;
+
+//task 2
+var myName = "Your Full Name";
+
+
+alert("As-habia");
+
+// Task 3
+var message;
+
+
+message = "Hello World";
+
+
+alert(message);
+
+
+
+// task 4
+var studentName = "Bella John";
+var age = 33;
+var gender = "Female";
+var address = "789 Rainbow Avenue, Wonderland";
+var contactNumber = "555-1234";
+
+
+alert("Student Name: " + studentName);
+alert("Age: " + age);
+alert("Gender: " + gender);
+alert("Address: " + address);
+alert("Contact Number: " + contactNumber);
+
+
+//task 5
+var alertMessage = "Hello World!\nWelcome to the world of JavaScript";
+
+alert(alertMessage);
+
+//task6
+var email = "bela@john.com";
+
+var alertMessage = "Email: " + email;
+
+alert(alertMessage);
+
+
+//task7
+var book = "A smarter way to learn JavaScript";
+
+alert("I am trying to learn from the book \"" + book + "\".");
+
+
+
+//task8
+var message = "JavaScript is a versatile language used primarily for web development, allowing for dynamic and interactive web pages. It's executed in web browsers and server-side platforms, enabling developers to add interactivity and functionality to their websites and web applications.";
+document.write(message +"<br/>");
+
+
+var specialString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+       
+        alert(specialString);
+        document.write(specialString)
+
